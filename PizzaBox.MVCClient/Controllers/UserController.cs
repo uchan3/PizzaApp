dc23e83048efdd656d1ee256d307f643e1cf21a3
urey chan <urey.chan3@gmail.com>
2019-12-23 11:52:39 -0600

    initial mvc commit with skeleton structure

---
+++ PizzaBox.MVCClient/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+namespace PizzaBox.MVCClient.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
